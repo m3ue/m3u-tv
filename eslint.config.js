6922b678f138e60ca133b495dceb3b8e0ed92c95
Shaun Parkison <shparkison@gmail.com>
2026-02-10 09:12:16 -0700

    feat: Add linting libraries

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,15 @@
-const prettier = require('eslint-plugin-prettier');
-
 module.exports = [
   {
+    ignores: [
+      '**/node_modules/**',
+      '**/android/**',
+      '**/ios/**',
+      '**/.expo/**',
+      'planby-native-pro-main/**',
+    ],
+  },
+  {
     files: ['**/*.ts', '**/*.tsx'],
-    ignores: ['node_modules/**', 'build/**', 'dist/**'],
     languageOptions: {
       parser: require('@typescript-eslint/parser'),
       parserOptions: {
@@ -13,28 +19,9 @@ module.exports = [
           jsx: true,
         },
       },
-      globals: {
-        React: 'readonly',
-      },
-    },
-    plugins: {
-      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
-      react: require('eslint-plugin-react'),
-      prettier: prettier,
     },
     rules: {
       semi: ['warn', 'always'],
-      'prefer-const': 'error',
-      'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      'prettier/prettier': [
-        'warn',
-        {
-          endOfLine: 'auto',
-          usePrettierrc: true,
-        },
-      ],
     },
   },
 ];
